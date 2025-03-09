9e51dd54822d2a20da8b33caa99a0e7d8d70dc35
Faggner04 <faculdadefagner@gmail.com>
2025-03-09 20:52:56 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade 1 Robinson/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a venda média mensal do produto: ");
+        int vendaMedia = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o preço atual do produto: ");
+        double precoAtual = double.Parse(Console.ReadLine());
+
+        double novoPreco = precoAtual;
+
+        if (vendaMedia < 500 && precoAtual < 30)
+        {
+            novoPreco += precoAtual * 0.10;
+        }
+        else if (vendaMedia >= 500 && precoAtual >= 30)
+        {
+            novoPreco -= precoAtual * 0.20;
+        }
+
+        Console.WriteLine($"O novo preço do produto é: R$ {novoPreco:F2}");
+    }
+}
