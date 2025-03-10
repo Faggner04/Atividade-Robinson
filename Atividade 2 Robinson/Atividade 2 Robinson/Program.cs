c249dd3e3e8941ec2e39ba91c2659b4530a3b984
Faggner04 <faculdadefagner@gmail.com>
2025-03-09 21:31:36 -0300

    Add files via upload

---
+++ Atividade 2 Robinson/Atividade 2 Robinson/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a altura em metros: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o sexo (M/F): ");
+        char sexo = char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        double pesoIdeal = sexo == 'M' ? (72.7 * altura) - 58 : (62.1 * altura) - 44.7;
+
+        Console.WriteLine($"O peso ideal é: {pesoIdeal:F2} kg");
+    }
+}
